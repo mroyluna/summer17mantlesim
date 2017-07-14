d1900d4cc5732f3b572411451667543ca368d7b4
Mousumi Roy <mousumi@MRMBPro.local>
2017-07-14 17:53:45 -0600

    removed extra folders

--- muon_gravity/points_by_regexp.m
+++
@@ -1,4 +0,0 @@
-function points = points_by_regexp(table, reg)
-    is_included = ~cellfun(@isempty, regexp(table.Properties.RowNames, reg));
-    points = table{is_included, Constants.xyz_index}';
-end
